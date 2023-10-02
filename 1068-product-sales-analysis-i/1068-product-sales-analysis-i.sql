0805d6d04dc1698f525652b43b769292e283c45e
Vaibhavi Sawant <96656695+VaibhaviSawant101@users.noreply.github.com>
2023-10-02 18:07:58 +0530

    Time: 3267 ms (7.20%), Space: 0B (100.00%) - LeetHub

---
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT Product.product_name, Sales.year, Sales.price
+FROM Sales
+LEFT JOIN Product ON Sales.product_id = Product.product_id
+ORDER BY Sales.sale_id;
