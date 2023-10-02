c0758e17cf79dba5e7bf550694c28646d68187f8
Vaibhavi Sawant <96656695+VaibhaviSawant101@users.noreply.github.com>
2023-10-02 17:47:07 +0530

    Time: 3187 ms (24.48%), Space: 0B (100.00%) - LeetHub

---
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT EmployeeUNI.unique_id, Employees.name
+FROM Employees
+LEFT JOIN EmployeeUNI ON EmployeeUNI.id = Employees.id
+ORDER BY Employees.name;
